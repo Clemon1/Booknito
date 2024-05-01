037ce2c6834c9b2dca1504230f9715310bb9f75c
Clemon Ezeh <clemonezeh@gmail.com>
2024-05-01 08:19:07 +0100

    added authSlice to redux store

--- admin/src/redux/store.ts
+++ admin/src/redux/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authenticateSliceReducer from "./authRedux/appSLice";
+import { authApi } from "./RTK_Query/authSlice";
 export const store = configureStore({
   reducer: {
     auth: authenticateSliceReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([authApi.middleware]),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
